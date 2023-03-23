eb8e9317b897442fa7b50caa41fe7e14ffb74081
MSeGa <segasr@outlook.in>
2023-03-23 23:02:42 +0530

    A1

--- streamlit_Trial.py
+++ streamlit_Trial.py
@@ -1,25 +1,14 @@
 # Streamlit Test File
 
-from openbb_terminal.sdk import openbb
-import pandas as pd
-# help(openbb.stocks.dd)
-from datetime import datetime
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy.stats import linregress
-from sklearn.linear_model import LinearRegression
-from scipy import stats
-import plotly.graph_objects as go
 import streamlit as st
-from PIL import Image
-import streamlit as st
-import Bullish_trendline
+#import Bullish_trendline
 
 st.title('Bullish Trendline Graph Generation')
 index=st.selectbox("Index",options=["NSE50","BSE"])
 if st.button('Generate'):
     with st.spinner("Extracting"):
-        Bullish_trendline.fun_getlist(index)
+        #Bullish_trendline.fun_getlist(index)
+        st.text("WIP")
 
 
 
